0c0a4b6b4f601a31bf1b6be0888b09f3ca0f638a
namkyu koo(구남규) <rnrb555@gmail.com>
2021-09-12 23:15:22 +0900

    [FIX]IOS빌드수정

--- src/screens/coachSelect/ChallengeSetting.js
+++ src/screens/coachSelect/ChallengeSetting.js
@@ -1,15 +1,15 @@
-import React, { useRef, useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import styled from "styled-components"
-import LongButton from "../../components/LongButton"
-import { Body1Text, H1Text, theme } from "../../styles/theme"
-import { coachColorVar } from "../../../apollo"
-import { useReactiveVar } from "@apollo/client"
-import { gql, useLazyQuery, useMutation } from "@apollo/client"
-import { stepVar } from "../../../apollo"
+import React, { useRef, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import LongButton from "../../components/LongButton";
+import { Body1Text, H1Text, theme } from "../../styles/theme";
+import { coachColorVar } from "../../../apollo";
+import { useReactiveVar } from "@apollo/client";
+import { gql, useLazyQuery, useMutation } from "@apollo/client";
+import { stepVar } from "../../../apollo";
 const ChallengeSetting = ({ swiperRef, navigation }) => {
-  const walkRef = useRef()
-  const coachColor = useReactiveVar(coachColorVar)
+  const walkRef = useRef();
+  const coachColor = useReactiveVar(coachColorVar);
   const {
     register,
     handleSubmit,
@@ -21,19 +21,19 @@ const ChallengeSetting = ({ swiperRef, navigation }) => {
     defaultValues: {
       walkingNum: 200,
     },
-  })
+  });
 
-  const [status, setStatus] = useState("")
-  const inputWatch = watch("walkingNum")
+  const [status, setStatus] = useState("");
+  const inputWatch = watch("walkingNum");
   // const walkStatus = walk
   // const coachStatus = coachColorVar().coach
   const handleGoToNext = () => {
     if (inputWatch < 200) {
-      return
+      return;
     }
-    swiperRef?.current.goToNext()
-    navigation.navigate("TabNavigator")
-  }
+    swiperRef?.current.goToNext();
+    navigation.navigate("TabNavigator");
+  };
 
   const PUT_CHALLENGE = gql`
     mutation putChallenge($challenge: ChallengeInput) {
@@ -43,13 +43,12 @@ const ChallengeSetting = ({ swiperRef, navigation }) => {
         challengeDate
       }
     }
-  `
-  const []
+  `;
   const [putChallengeMutation, { data }] = useMutation(PUT_CHALLENGE, {
-    onCompleted: data => {
-      console.log(data, "data")
+    onCompleted: (data) => {
+      console.log(data, "data");
     },
-  })
+  });
 
   useEffect(() => {
     putChallengeMutation({
@@ -60,8 +59,8 @@ const ChallengeSetting = ({ swiperRef, navigation }) => {
           challengeDate: "2021-02-11",
         },
       },
-    })
-  }, [])
+    });
+  }, []);
 
   const defaultWalking = () => {
     //  hometabbutton > 색 black으로, 문자 오늘은 그말할래요로..Longbutton 어떡하지..?
@@ -74,7 +73,7 @@ const ChallengeSetting = ({ swiperRef, navigation }) => {
     //   },
     // })
     // coachStatus.concat(walkStatus)
-  }
+  };
 
   return (
     <>
@@ -92,7 +91,7 @@ const ChallengeSetting = ({ swiperRef, navigation }) => {
             ref={walkRef}
             keyboardType="number-pad"
             defaultValue="200"
-            onChangeText={text => setValue("walkingNum", text)}
+            onChangeText={(text) => setValue("walkingNum", text)}
             onSubmitEditing={handleSubmit(handleGoToNext)}
             caretHidden={true}
             returnKeyType="next"
@@ -107,14 +106,15 @@ const ChallengeSetting = ({ swiperRef, navigation }) => {
             handleGoToNext={handleSubmit(handleGoToNext)}
             disabled={inputWatch < 200}
             // characterStatus={setStatus("walk")}
-            btnBackColor={coachColorVar()?.color?.main}>
+            btnBackColor={coachColorVar()?.color?.main}
+          >
             저장
           </LongButton>
         </LongBox>
       </Container>
     </>
-  )
-}
+  );
+};
 
 const InputBox = styled.View`
   width: 100%;
@@ -123,12 +123,12 @@ const InputBox = styled.View`
   flex-direction: row;
   align-items: center;
   margin-bottom: 30px;
-`
+`;
 const LongBox = styled.View`
   width: 80%;
   height: 15%;
   flex-direction: row;
-`
+`;
 
 const TodayChallengeBox = styled.View`
   width: 100%;
@@ -136,28 +136,28 @@ const TodayChallengeBox = styled.View`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const WalkTextInput = styled.TextInput`
   font-size: 64px;
   font-weight: 700;
   text-align: center;
   width: 50%;
-  border-bottom-color: ${props => props.coachColor.color.main};
+  border-bottom-color: ${(props) => props.coachColor.color.main};
   border-bottom-width: 2px;
   margin-right: 10px;
-`
+`;
 const ChallengeText = styled(H1Text)`
   text-align: center;
   color: ${theme.grayScale.gray1};
-`
+`;
 const WalkiText = styled(Body1Text)`
   text-align: center;
   color: ${theme.TextColor};
-`
+`;
 const Container = styled.SafeAreaView`
   align-items: center;
   width: 100%;
   height: 100%;
-`
-export default ChallengeSetting
+`;
+export default ChallengeSetting;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,141 +1,119 @@
-import React, { useState, useEffect } from "react"
-import { View, Text, TouchableOpacity } from "react-native"
-import styled from "styled-components"
-import * as Location from "expo-location"
-import axios from "axios"
-import Loading from "../../components/Loading"
-import WeatherLogo from "../../../assets/icons/sun.png"
-import SpaceLogo from "../../../assets/icons/bar.png"
-import toki_default from "../../../assets/images/character/toki_hi.png"
-import buki_default from "../../../assets/images/character/buki.png"
-import toki_walking from "../../../assets/images/character/toki_walking.png"
-import buki_walking from "../../../assets/images/character/buki_walking.png"
-import { CircularProgress } from "react-native-svg-circular-progress"
-import { Body3Text, H3Text, H4Text, theme } from "../../styles/theme"
-import LongButton from "../../components/LongButton"
-import { coachColorVar } from "../../../apollo"
-import BlurOverlay, {
-  closeOverlay,
-  openOverlay,
-} from "react-native-blur-overlay"
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+import * as Location from "expo-location";
+import axios from "axios";
+import Loading from "../../components/Loading";
+import WeatherLogo from "../../../assets/icons/sun.png";
+import SpaceLogo from "../../../assets/icons/bar.png";
+import toki_default from "../../../assets/images/character/toki_hi.png";
+import buki_default from "../../../assets/images/character/buki.png";
+import toki_walking from "../../../assets/images/character/toki_walking.png";
+import buki_walking from "../../../assets/images/character/buki_walking.png";
+import { CircularProgress } from "react-native-svg-circular-progress";
+import { Body3Text, H3Text, H4Text, theme } from "../../styles/theme";
+import LongButton from "../../components/LongButton";
+import { coachColorVar } from "../../../apollo";
 
-import { request, PERMISSIONS, check } from "react-native-permissions"
+import { request, PERMISSIONS, check } from "react-native-permissions";
 
 const Home = ({ navigation }) => {
-  const [state, setState] = useState([])
-  const [cateState, setCateState] = useState([])
-  const [ready, setReady] = useState(true)
-  const [character, setCharacter] = useState("")
+  const [state, setState] = useState([]);
+  const [cateState, setCateState] = useState([]);
+  const [ready, setReady] = useState(true);
+  const [character, setCharacter] = useState("");
   const [weather, setWeather] = useState({
     temp: 1,
     condition: "맑음",
-  })
+  });
 
   const getSteps = () => {
-    Pedometer.watchStepCount(result =>
-      setSteps(steps => ({
+    Pedometer.watchStepCount((result) =>
+      setSteps((steps) => ({
         ...steps,
         currentStepCount: result.steps,
       }))
-    )
-  }
+    );
+  };
   const [steps, setSteps] = useState({
     isPedometerAvailable: "checking",
     pastStepCount: 0,
     currentStepCount: 0,
-  })
+  });
 
   useEffect(() => {
-    request(PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION).then(granted => {
+    request(PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION).then((granted) => {
       if (granted) {
-        console.log(granted)
-        getSteps()
+        console.log(granted);
+        getSteps();
       }
-    })
-  }, [])
+    });
+  }, []);
 
-  const { currentStepCount, isPedometerAvailable } = steps
+  const { currentStepCount, isPedometerAvailable } = steps;
 
-  const Location = "강남구"
-  const percentage = 66
-  const color = coachColorVar().color.main
+  const Location = "강남구";
+  const percentage = 66;
+  const color = coachColorVar().color.main;
 
-  const [currentDate, setcurrentDate] = useState("")
-  const [currentTime, setcurrentTime] = useState("")
+  const [currentDate, setcurrentDate] = useState("");
+  const [currentTime, setcurrentTime] = useState("");
 
   useEffect(() => {
-    let date = new Date().getDate()
-    let month = new Date().getMonth() + 1
-    let hours = new Date().getHours()
-    let min = new Date().getMinutes()
-    console.log(hours)
-    setcurrentDate(month + "월" + " " + date + "일")
-    setcurrentTime(hours + ":" + min + "PM")
+    let date = new Date().getDate();
+    let month = new Date().getMonth() + 1;
+    let hours = new Date().getHours();
+    let min = new Date().getMinutes();
+    console.log(hours);
+    setcurrentDate(month + "월" + " " + date + "일");
+    setcurrentTime(hours + ":" + min + "PM");
     setTimeout(() => {
-      setState()
-      setCateState()
-      getLocation()
-      setReady(false)
-    }, 1000)
-  }, [])
+      setState();
+      setCateState();
+      getLocation();
+      setReady(false);
+    }, 1000);
+  }, []);
 
   const handleGoToNext = () => {
     // swiperRef?.current.goToNext();
-    navigation.navigate("ChallengeSetting")
-  }
+    navigation.navigate("ChallengeSetting");
+  };
 
   const instructions = Platform.select({
     ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
     android:
       "Double tap R on your keyboard to reload,\n" +
       "Shake or press menu button for dev menu",
-  })
-  const renderBlurChilds = () => {
-    return (
-      <View>
-        <Text
-          style={{
-            fontSize: 50,
-            fontWeight: "bold",
-            color: coachColorVar().color.main,
-            alignItems: "center",
-            marginLeft: 45,
-          }}>
-          {currentStepCount}
-          {"\n"}
-        </Text>
+  });
 
-        <Text>{"\n"}목표를 설정해 주세요</Text>
-      </View>
-    )
-  }
   const getLocation = async () => {
     try {
-      await Location.requestForegroundPermissionsAsync()
-      const locationData = await Location.getCurrentPositionAsync()
-      const latitude = locationData["coords"]["latitude"]
-      const longitude = locationData["coords"]["longitude"]
+      await Location.requestForegroundPermissionsAsync();
+      const locationData = await Location.getCurrentPositionAsync();
+      const latitude = locationData["coords"]["latitude"];
+      const longitude = locationData["coords"]["longitude"];
 
       // const API_KEY = "cfc258c75e1da2149c33daffd07a911d"
-      const API_KEY = "5f45ba75e045a8cb44f05067fb179d01"
+      const API_KEY = "5f45ba75e045a8cb44f05067fb179d01";
       const result = await axios.get(
         `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-      )
+      );
 
-      const temp = result.data.main.temp
-      const condition = result.data.weather[0].main
-      console.log(result)
-      console.log(temp)
-      console.log(condition)
+      const temp = result.data.main.temp;
+      const condition = result.data.weather[0].main;
+      console.log(result);
+      console.log(temp);
+      console.log(condition);
 
       setWeather({
         temp,
         condition,
-      })
+      });
     } catch (error) {
       // Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?")
     }
-  }
+  };
   return ready ? (
     <Loading />
   ) : (
@@ -159,7 +137,8 @@ const Home = ({ navigation }) => {
           <WeatherSpace>
             <WeatherImage
               source={WeatherLogo}
-              resizeMode={"contain"}></WeatherImage>
+              resizeMode={"contain"}
+            ></WeatherImage>
             <CurrentWeather>{weather.condition}</CurrentWeather>
           </WeatherSpace>
         </WeatherStatus>
@@ -171,7 +150,8 @@ const Home = ({ navigation }) => {
               percentage={percentage}
               donutColor={color}
               size={300}
-              progressWidth={140}>
+              progressWidth={140}
+            >
               <CharacterBox>
                 <CharacetrImage
                   // default, walking, fail, completed
@@ -184,32 +164,6 @@ const Home = ({ navigation }) => {
                   resizeMode="contain"
                 />
               </CharacterBox>
-              <TouchableOpacity
-                onPress={() => {
-                  openOverlay()
-                }}
-                style={{
-                  width: "50%",
-                  height: 36,
-                  backgroundColor: "#FFF",
-                  borderRadius: 4,
-                  margin: 16,
-                }}
-              />
-              <BlurOverlay
-                radius={260}
-                downsampling={4}
-                brightness={-450}
-                onPress={() => {
-                  closeOverlay()
-                }}
-                customStyles={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                blurStyle="dark"
-                children={renderBlurChilds()}
-              />
             </CircularProgress>
           </ProgressGoal>
         </GoalBox>
@@ -222,45 +176,45 @@ const Home = ({ navigation }) => {
         </LongButton>
       </BottomStatus>
     </Container>
-  )
-}
+  );
+};
 
 const blurgoal = styled.Text`
   font-size: 25;
   font-weight: 700;
   color: ${coachColorVar().color.main};
-`
+`;
 const ProgressGoal = styled(CircularProgress)`
   width: 292px;
   height: 292px;
-`
+`;
 
 const GoalBox = styled.View`
   width: 100%;
   height: 100%;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const CharacterBox = styled.View`
   width: 120px;
   height: 192px;
-`
+`;
 
 const CharacetrImage = styled.Image`
   width: 120px;
   height: 192px;
-`
+`;
 
 const CheerText = styled.Text`
   font-size: 16px;
-`
+`;
 
 const Container = styled.SafeAreaView`
   align-items: center;
   width: 100%;
   height: 100%;
-`
+`;
 
 const GoalContainer = styled.TouchableOpacity`
   width: 300px;
@@ -270,12 +224,12 @@ const GoalContainer = styled.TouchableOpacity`
   color: ${theme.grayScale.white};
   background-color: ${theme.toki.color.main};
   border-radius: 8px;
-`
+`;
 
 const GoalText = styled(H4Text)`
   color: ${theme.grayScale.white};
   text-align: center;
-`
+`;
 
 const TopStatus = styled.View`
   width: 100%;
@@ -286,29 +240,29 @@ const TopStatus = styled.View`
   padding-right: 1px;
   padding-top: 35px;
   flex-direction: row;
-`
+`;
 
 const TimeStatus = styled.View`
   width: 40%;
   height: 90%;
   padding-left: 15px;
-`
+`;
 const WeatherStatus = styled.View`
   width: 60%;
   height: 100%;
   flex-direction: row;
   padding-left: 50px;
-`
+`;
 const LocationSpace = styled.View`
   width: 30%;
   align-items: center;
   justify-content: center;
-`
+`;
 const WeatherSpace = styled.View`
   width: 30%;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const BarSpace = styled.View`
   width: 10%;
@@ -317,20 +271,20 @@ const BarSpace = styled.View`
   padding-bottom: 40px;
   padding-left: 30px;
   padding-right: 10px;
-`
+`;
 const CurrentDate = styled.Text`
   width: 100px;
   height: 20px;
   font-weight: bold;
   font-size: 16px;
-`
+`;
 
 const CurrentTime = styled.Text`
   width: 130px;
   height: 40px;
   font-weight: bold;
   font-size: 30px;
-`
+`;
 
 const CurrentWeather = styled.Text`
   width: 70px;
@@ -339,13 +293,13 @@ const CurrentWeather = styled.Text`
   padding-left: 25px;
   padding-top: 5px;
   color: #828282;
-`
+`;
 const CurrentTemperature = styled.Text`
   width: 70px;
   height: 30px;
   padding-left: 30px;
   font-size: 26px;
-`
+`;
 
 const CurrentLocation = styled.Text`
   width: 70px;
@@ -354,7 +308,7 @@ const CurrentLocation = styled.Text`
   padding-left: 30px;
   padding-top: 5px;
   color: #828282;
-`
+`;
 
 const WeatherImage = styled.Image`
   width: 100px;
@@ -362,14 +316,14 @@ const WeatherImage = styled.Image`
   padding-top: 40px;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const BarImage = styled.Image`
   width: 50px;
   height: 50px;
   align-items: center;
   justify-content: center;
-`
+`;
 const MiddleStatus = styled.View`
   width: 100%;
   height: 60%;
@@ -379,7 +333,7 @@ const MiddleStatus = styled.View`
   padding-right: 1px;
   padding-top: 15px;
   flex-direction: row;
-`
+`;
 const BottomStatus = styled.View`
   width: 80%;
   height: 20%;
@@ -387,6 +341,6 @@ const BottomStatus = styled.View`
   justify-content: flex-start;
   padding-top: 30px;
   flex-direction: row;
-`
+`;
 
-export default Home
+export default Home;
