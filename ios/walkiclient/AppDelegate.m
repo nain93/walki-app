47fe456aab36234ec0ce6e1863a00d44215e4d1c
Jiha kim <jihakim@Jihaui-MacBookPro.local>
2021-11-03 14:56:59 +0900

    ipa setting

--- ios/walkiclient/AppDelegate.m
+++ ios/walkiclient/AppDelegate.m
@@ -5,7 +5,6 @@
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
 #import <RNKakaoLogins.h>
-#import <Firebase.h>
 
 #import <UMCore/UMModuleRegistry.h>
 #import <UMReactNativeAdapter/UMNativeModulesProxy.h>
@@ -43,12 +42,6 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  if ([FIRApp defaultApp] == nil){
-    [FIRApp configure];
-  }
-#if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
-  InitializeFlipper(application);
-#endif
   
   self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
   self.launchOptions = launchOptions;
